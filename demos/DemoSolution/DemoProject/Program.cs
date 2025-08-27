5f251e03393775faec12a840fa67f7e43065b9cb
JP <jp@jp.nl>
2025-08-27 14:05:48 +0200

    ThreadPool

--- demos/DemoSolution/DemoProject/Program.cs
+++ demos/DemoSolution/DemoProject/Program.cs
@@ -5,4 +5,12 @@ using DemoProject;
 Console.WriteLine("Hello, World!");
 
 // Basics.Go();
-RaceCondition.Go();
+// RaceCondition.Go();
+
+ThreadPool.QueueUserWorkItem((q) =>
+{
+	Thread.Sleep(1000);
+	Console.WriteLine("hey werk klaar!");
+});
+
+Console.ReadKey();
