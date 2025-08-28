c9d8c0d37e04746409e9826b877df4dbd1ab31fc
JP <jp@jp.nl>
2025-08-28 15:00:07 +0200

    async/await, APM met Begin-/End-methoden

---
+++ demos/DemoSolution/AsyncDemo/AsyncSpullen.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace AsyncDemo;
+
+public class AsyncSpullen
+{
+	private static object locKObj = new { };
+
+	public static async Task Go()
+	{
+		var path = "C:\\repos\\course-instances\\threads-2025-08\\bestand.txt";
+		// lock (locKObj)
+		// {
+			await File.AppendAllTextAsync(path, "Hallo!");
+		// }
+
+		Console.WriteLine("yes werkt");
+
+		// Asynchronous Programming Model
+		// .Begin...
+		// .End...
+		// var stream = File.OpenWrite(path);
+		//
+		// var buffer = Encoding.ASCII.GetBytes("wow dit werkt!!");
+		//
+		// stream.BeginWrite(buffer, 0, buffer.Length, new AsyncCallback(FinishWrite), stream);
+	}
+
+	// public static void FinishWrite(IAsyncResult asyncResult)
+	// {
+	// 	var stream = asyncResult.AsyncState as FileStream;
+	// 	stream.EndWrite(asyncResult);
+	// 	stream.Flush();
+	// 	stream.Close();
+	// 	Console.WriteLine("Bestand nu echt weggeschreven!");
+	// }
+}
---
+++ demos/DemoSolution/AsyncDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+using AsyncDemo;
+
+Console.WriteLine("Hello, World!");
+
+
+AsyncSpullen.Go().GetAwaiter().GetResult();
+
+// (typeof AsyncSpullen)
+Process.Start("cmd.exe");
